48caaaecbebacb013dc8f2c5159ee213e471db63
wjr21 <wjr21@sussex.ac.uk>
2023-02-21 15:11:18 +0000

    Debugging

--- cosmic_sfr_density.py
+++ cosmic_sfr_density.py
@@ -14,7 +14,7 @@ data = load(sys.argv[1])
 zs = (1 / data.stars.birth_scale_factors.value) - 1
 print(zs.min(), zs.max())
 # Create age bins
-age_bins = np.arange(0, cosmo.age(30).to(u.Gyr).value, 0.1) * u.Gyr
+age_bins = np.arange(cosmo.age(30).to(u.Gyr).value, 14, 0.1) * u.Gyr
 print(age_bins)
 bin_edges = z_at_value(cosmo.age, age_bins, zmin=0, zmax=50)
 bin_cents = (bin_edges[1:] + bin_edges[:-1]) / 2
