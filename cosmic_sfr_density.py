4c27bf85608fd54d0b868e35de81cfabeec3f7e3
wjr21 <wjr21@sussex.ac.uk>
2023-02-22 13:03:23 +0000

    Plotting multiple bovine fractions

--- cosmic_sfr_density.py
+++ cosmic_sfr_density.py
@@ -1,3 +1,7 @@
+"""
+run with:
+python cosmic_sfr_density.py 0036 <directories...>
+"""
 import sys
 import numpy as np
 from swiftsimio import load
@@ -7,27 +11,9 @@ from astropy.cosmology import z_at_value
 import astropy.units as u
 
 
-# Load data
-data = load(sys.argv[1])
-
-# Get redshift of stellar birth
-zs = (1 / data.stars.birth_scale_factors.value) - 1
-print(zs.min(), zs.max())
-# Create age bins
-age_bins = np.arange(cosmo.age(30).to(u.Gyr).value, 14, 0.1) * u.Gyr
-print(age_bins)
-bin_edges = z_at_value(cosmo.age, age_bins, zmin=-1, zmax=50)[::-1]
-bin_cents = (bin_edges[1:] + bin_edges[:-1]) / 2
-mass_formed = np.zeros(bin_cents.size)
-
-# Bin the stars
-H, _ = np.histogram(zs, bins=bin_edges, weights=data.stars.masses)
-
-# Convert the mass sum in H to SFR in M_sun / Myr
-sfr = H / 100
-
-# Convert to cSFRD in M_sun / Myr / Mpc^3
-csfrd = sfr / (12.5 ** 3)
+# Get command line args
+snap = sys.argv[1]
+paths = sys.argv[2:]
 
 # Set up plot
 fig = plt.figure()
@@ -35,11 +21,39 @@ ax = fig.add_subplot(111)
 ax.semilogy()
 ax.grid(True)
 
-# Plot curve
-ax.plot(bin_cents, csfrd)
+for path in paths:
+
+    # Load data
+    data = load(path + "/cowshed12_<snap>.hdf5".replace("<snap>", snap))
+
+    # Get the label for this run
+    f_bov = float(path.split("/")[-1][2:].replace("p", "."))
+    lab = "$f_\mathrm{bovine}=%.2f$ % f_bov"
+
+    # Get redshift of stellar birth
+    zs = (1 / data.stars.birth_scale_factors.value) - 1
+    print(zs.min(), zs.max())
+    # Create age bins
+    age_bins = np.arange(cosmo.age(30).to(u.Gyr).value, 14, 0.1) * u.Gyr
+    print(age_bins)
+    bin_edges = z_at_value(cosmo.age, age_bins, zmin=-1, zmax=50)[::-1]
+    bin_cents = (bin_edges[1:] + bin_edges[:-1]) / 2
+    mass_formed = np.zeros(bin_cents.size)
+
+    # Bin the stars
+    H, _ = np.histogram(zs, bins=bin_edges, weights=data.stars.masses)
+
+    # Convert the mass sum in H to SFR in M_sun / Myr
+    sfr = H / 100
+
+    # Convert to cSFRD in M_sun / Myr / Mpc^3
+    csfrd = sfr / (12.5 ** 3)
+
+    # Plot curve
+    ax.plot(bin_cents, csfrd, label=lab)
 
 # Label axes
-ax.set_xlabel("Age / [Myr]")
+ax.set_xlabel("$z$")
 ax.set_ylabel("CSFRD / [M$_\odot$ / Myr / Mpc$^{3}$]")
 
 fig.savefig("../plots/csfrd.png", bbox_inches="tight", dpi=100)
